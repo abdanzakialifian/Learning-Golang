eb800fd09b9c78cccc4a2ba2ba09080a08ca0bf4
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2025-02-19 21:09:13 +0700

    learning-string-golang

---
+++ Golang Basic/learning-string-golang/go.mod
@@ -0,0 +1,3 @@
+module learning-string-golang
+
+go 1.23.0
---
+++ Golang Basic/learning-string-golang/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Abdan Zaki Alifian")
+	fmt.Println("Length First :", len("Abdan Zaki Alifian"))
+	fmt.Println("Length Second :", len("Abdan Zaki Alifian  "))
+	fmt.Println("First Character :", string("Abdan Zaki Alifian"[0]))
+}
