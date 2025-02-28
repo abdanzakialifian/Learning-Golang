252f4475be8c809a80b4adc96e7c8efac95ff8c7
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2025-02-28 08:15:16 +0700

    learning recursive function golang

--- Golang Basic/learning-function-golang/main.go
+++ Golang Basic/learning-function-golang/main.go
@@ -61,6 +61,11 @@ func main() {
 	registerUser("Anjing", func(name string) bool {
 		return name == "Anjing"
 	})
+
+	fmt.Println("===================")
+
+	fmt.Println(factorialLoop(10))
+	fmt.Println(factorialRecursive(10))
 }
 
 func sayHello() {
@@ -127,5 +132,23 @@ func registerUser(name string, isBlacklist Blacklist) {
 		fmt.Println("You are blocked", name)
 	} else {
 		fmt.Println("Welcome", name)
+	}
+}
+
+func factorialLoop(value int) int {
+	result := 1
+
+	for i := value; i > 0; i-- {
+		result *= i
+	}
+
+	return result
+}
+
+func factorialRecursive(value int) int {
+	if value == 1 {
+		return 1
+	} else {
+		return value * factorialRecursive(value-1)
 	}
 }
