7493a182f4b19a73012cd9708713a592e16d8e58
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2025-03-01 14:44:34 +0700

    learning comment golang

---
+++ Golang Basic/learning-comment-golang/go.mod
@@ -0,0 +1,3 @@
+module learning-comment-golang
+
+go 1.23.0
---
+++ Golang Basic/learning-comment-golang/main.go
@@ -0,0 +1,9 @@
+package main
+
+/*
+This is multiline comment
+You can add a lot of comments here
+*/
+func main() {
+	// This is single line comment
+}
