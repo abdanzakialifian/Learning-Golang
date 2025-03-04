79952acfeda9b5f01c3d16badd43bedd0b4dd17a
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2025-03-05 06:26:06 +0700

    learning package math golang

---
+++ Golang Standard Library/learning-package-math-golang/go.mod
@@ -0,0 +1,3 @@
+module learning-package-math-golang
+
+go 1.23.0
---
+++ Golang Standard Library/learning-package-math-golang/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Ceil(1.30))
+	fmt.Println(math.Floor(1.70))
+	fmt.Println(math.Round(1.59))
+	fmt.Println(math.Max(10, 11))
+	fmt.Println(math.Min(10, 11))
+}
