e39462663a8e56b043dc74441d5d2053f21b4afe
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2025-03-16 16:21:57 +0700

    learning sync golang (sync rwmutex)

--- Golang Goroutines/learning-sync-golang/test/sync_mutex_test.go
+++ Golang Goroutines/learning-sync-golang/test/sync_mutex_test.go
@@ -23,3 +23,36 @@ func TestSyncMutex(t *testing.T) {
 	time.Sleep(5 * time.Second)
 	fmt.Println("Counter : ", x)
 }
+
+func TestSyncRWMutex(t *testing.T) {
+	account := BankAccount{}
+	for range 100 {
+		go func() {
+			for range 100 {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance : ", account.GetBalance())
+}
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(balance int) {
+	account.RWMutex.Lock()
+	account.Balance = account.Balance + balance
+	account.RWMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutex.RLock()
+	balance := account.Balance
+	account.RWMutex.RUnlock()
+	return balance
+}
