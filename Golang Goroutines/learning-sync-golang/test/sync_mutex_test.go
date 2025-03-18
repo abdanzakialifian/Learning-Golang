44e224315b1f0b3af4ab31f61a1e27197bdd5b96
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2025-03-18 12:44:36 +0700

    learning sync golang (wait group)

--- Golang Goroutines/learning-sync-golang/test/sync_mutex_test.go
+++ Golang Goroutines/learning-sync-golang/test/sync_mutex_test.go
@@ -111,3 +111,23 @@ func Transfer(user1 *UserBalance, user2 *UserBalance, amount int) {
 	user1.Unlock()
 	user2.Unlock()
 }
+
+func TestWaitGroup(t *testing.T) {
+	group := sync.WaitGroup{}
+
+	for range 100 {
+		go RunAsynchronous(&group)
+	}
+
+	group.Wait()
+	fmt.Println("Complete.")
+}
+
+func RunAsynchronous(group *sync.WaitGroup) {
+	defer group.Done()
+
+	group.Add(1)
+
+	fmt.Println("Hello")
+	time.Sleep(1 * time.Second)
+}
