3634773ca21048510fa2dc28ad102427ddd1a915
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2025-03-22 15:16:30 +0700

    learning atomic golang

--- Golang Goroutines/learning-sync-golang/test/sync_mutex_test.go
+++ Golang Goroutines/learning-sync-golang/test/sync_mutex_test.go
@@ -3,7 +3,6 @@ package test
 import (
 	"fmt"
 	"sync"
-	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -253,22 +252,4 @@ func TestCondBroadcast(t *testing.T) {
 	}()
 
 	group.Wait()
-}
-
-func TestAtomic(t *testing.T) {
-	group := sync.WaitGroup{}
-	var counter int64 = 0
-
-	for i := 1; i <= 1000; i++ {
-		group.Add(1)
-		go func() {
-			defer group.Done()
-			for j := 1; j <= 100; j++ {
-				atomic.AddInt64(&counter, 1)
-			}
-		}()
-	}
-
-	group.Wait()
-	fmt.Println("Counter = ", counter)
 }
