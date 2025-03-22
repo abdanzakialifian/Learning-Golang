f24e26d9faaaca08b0264b092a5f9c3253b6d089
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2025-03-22 16:22:22 +0700

    learning timer golang (tiker)

---
+++ Golang Goroutines/learning-timer-golang/test/ticker_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestTicker(t *testing.T) {
+	ticker := time.NewTicker(1 * time.Second)
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		ticker.Stop()
+	}()
+
+	for ticker := range ticker.C {
+		fmt.Println(ticker)
+	}
+}
+
+func TestTick(t *testing.T) {
+	channel := time.Tick(1 * time.Second)
+
+	for time := range channel {
+		fmt.Println(time)
+	}
+}
