9777597ab7f87a7b0d946d19cd33d165e18407e6
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2025-03-22 14:55:52 +0700

    learning sync golang (sync cond)

--- Golang Goroutines/learning-sync-golang/test/sync_mutex_test.go
+++ Golang Goroutines/learning-sync-golang/test/sync_mutex_test.go
@@ -208,3 +208,48 @@ func TestMap(t *testing.T) {
 
 	fmt.Println("Complete.")
 }
+
+var locker = sync.Mutex{}
+var cond = sync.NewCond(&locker)
+var group = sync.WaitGroup{}
+
+func WaitCondition(value int) {
+	defer group.Done()
+
+	cond.L.Lock()
+	cond.Wait()
+
+	fmt.Println("Done ", value)
+
+	cond.L.Unlock()
+}
+
+func TestCondSignal(t *testing.T) {
+	for i := range 10 {
+		group.Add(1)
+		go WaitCondition(i + 1)
+	}
+
+	go func() {
+		for range 10 {
+			time.Sleep(1 * time.Second)
+			cond.Signal()
+		}
+	}()
+
+	group.Wait()
+}
+
+func TestCondBroadcast(t *testing.T) {
+	for i := range 10 {
+		group.Add(1)
+		go WaitCondition(i + 1)
+	}
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		cond.Broadcast()
+	}()
+
+	group.Wait()
+}
