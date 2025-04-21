2ee113dca01a161f53562a552b6f8b241f950b47
Abdan Zaki Alifian <abdan.alifian@cimbniaga.co.id>
2025-04-21 07:37:53 +0700

    learning golang database (database pooling)

---
+++ Golang Database/learning-golang-database/test/database.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"database/sql"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/learning_golang_database")
+	if err != nil {
+		panic(err)
+	}
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	return db
+}
