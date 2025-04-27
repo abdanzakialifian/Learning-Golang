1b1ded8b874d6a55a6a1651be67ddb0c75b1ab32
Abdan Zaki Alifian <abdan.alifian@cimbniaga.co.id>
2025-04-27 09:14:49 +0700

    learning golang web (handler)

---
+++ Golang Web/learning-golang-web/test/handler_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprint(w, "Hello Golang")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
