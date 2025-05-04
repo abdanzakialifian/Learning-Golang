b354922aa589a01d7c2fe0a89880d54ba8d3b372
Abdan Zaki Alifian <abdan.alifian@cimbniaga.co.id>
2025-05-04 16:36:39 +0700

    learning golang web (download file)

---
+++ Golang Web/learning-golang-web/test/download_test.go
@@ -0,0 +1,31 @@
+package test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestDownloadFile(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(downloadFile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func downloadFile(writer http.ResponseWriter, request *http.Request) {
+	file := request.URL.Query().Get("file")
+
+	if file == "" {
+		writer.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(writer, "Bad Request")
+		return
+	}
+
+	writer.Header().Add("Content-Disposition", "attachment; filename=\""+file+"\"")
+	http.ServeFile(writer, request, "resources/"+file)
+}
