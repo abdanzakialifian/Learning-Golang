25eb97815b296e0f1926dcb8c4ec8fc8af8f9702
Abdan Zaki Alifian <abdan.alifian@cimbniaga.co.id>
2025-05-07 07:22:09 +0700

    learning json golang (json object test)

---
+++ Golang JSON/learning-json-golang/test/json_object_test.go
@@ -0,0 +1,30 @@
+package test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+type Customer struct {
+	FirstName  string
+	MiddleName string
+	LastName   string
+	Age        int
+	Married    bool
+}
+
+func TestJsonObject(t *testing.T) {
+	customer := Customer{
+		FirstName:  "Abdan",
+		MiddleName: "Zaki",
+		LastName:   "Alifian",
+		Age:        25,
+		Married:    true,
+	}
+	bytes, err := json.Marshal(customer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bytes))
+}
