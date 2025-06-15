e17c7a7c75787b7f745a6a9d4589a3b8aa56b4d9
Abdan Zaki Alifian <abdan.alifian@cimbniaga.co.id>
2025-06-15 22:59:15 +0700

    learning orm golang (embedded struct)

--- Golang ORM/learning-orm-golang/test/user.go
+++ Golang ORM/learning-orm-golang/test/user.go
@@ -4,11 +4,17 @@ import "time"
 
 type User struct {
 	Id          string    `gorm:"column:id;primary_key;<-:create"` // "<-:create is field permission"
-	Name        string    `gorm:"column:name"`
+	Name        Name      `gorm:"embedded"`
 	Password    string    `gorm:"column:password"`
 	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime;<-:create"` // "<-:create is field permission"
 	UpdatedAt   time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
 	Information string    `gorm:"column:information;-"` // "- is field permission"
+}
+
+type Name struct {
+	FirstName  string `gorm:"column:first_name"`
+	MiddleName string `gorm:"column:middle_name"`
+	LastName   string `gorm:"column:last_name"`
 }
 
 func (user *User) TableName() string {
