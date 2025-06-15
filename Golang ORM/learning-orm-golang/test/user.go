859655096386e744bc2fedc48879dafa5118aa4b
Abdan Zaki Alifian <abdan.alifian@cimbniaga.co.id>
2025-06-15 18:00:20 +0700

    learning orm golang (field permission)

--- Golang ORM/learning-orm-golang/test/user.go
+++ Golang ORM/learning-orm-golang/test/user.go
@@ -3,11 +3,12 @@ package test
 import "time"
 
 type User struct {
-	Id        string    `gorm:"column:id;primary_key"`
-	Name      string    `gorm:"column:name"`
-	Password  string    `gorm:"column:password"`
-	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	Id          string    `gorm:"column:id;primary_key;<-:create"` // "<-:create is field permission"
+	Name        string    `gorm:"column:name"`
+	Password    string    `gorm:"column:password"`
+	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime;<-:create"` // "<-:create is field permission"
+	UpdatedAt   time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	Information string    `gorm:"column:information;-"` // "- is field permission"
 }
 
 func (user *User) TableName() string {
