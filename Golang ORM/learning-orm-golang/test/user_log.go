8273ed4d85ff8e74d7da0dbe0aa487fa01d3570f
Abdan Zaki Alifian <abdan.alifian@cimbniaga.co.id>
2025-06-18 07:03:42 +0700

    learning orm golang (timestamp tracking)

--- Golang ORM/learning-orm-golang/test/user_log.go
+++ Golang ORM/learning-orm-golang/test/user_log.go
@@ -1,13 +1,11 @@
 package test
 
-import "time"
-
 type UserLog struct {
-	Id        int       `gorm:"column:id;primary_key;autoIncrement"`
-	UserId    string    `gorm:"column:user_id"`
-	Action    string    `gorm:"column:action"`
-	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	Id        int    `gorm:"column:id;primary_key;autoIncrement"`
+	UserId    string `gorm:"column:user_id"`
+	Action    string `gorm:"column:action"`
+	CreatedAt int64  `gorm:"column:created_at;autoCreateTime:milli"`
+	UpdatedAt int64  `gorm:"column:updated_at;autoCreateTime:milli;autoUpdateTime:milli"`
 }
 
 func (l *UserLog) TableName() string {
